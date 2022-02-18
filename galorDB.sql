1b31b8e99a6c8d0dd8234a6abb8c79492f3613ae
Neiroh <raulito72001@gmail.com>
2022-02-18 09:21:26 +0100

    base de datos

--- galorDB.sql
+++ galorDB.sql
@@ -11,3 +11,33 @@ CREATE TABLE IF NOT EXISTS user(
     correoUser VARCHAR(200) NOT NULL,
     fotoPerfil LONGBLOB
 );
+
+CREATE TABLE IF NOT EXISTS img(
+    idImg INT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS likes(
+    idImg INT NOT NULL,
+    idUser INT NOT NULL,
+    numLikes INT,
+    PRIMARY KEY (idImg, idUser),
+    FOREIGN KEY (idImg) REFERENCES img(idImg)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (idUser) REFERENCES user(idUser)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS coment(
+    idImg INT NOT NULL,
+    idUser INT NOT NULL,
+    text VARCHAR(10000) NOT NULL,
+    PRIMARY KEY (idImg, idUser),
+    FOREIGN KEY (idImg) REFERENCES img(idImg)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (idUser) REFERENCES user(idUser)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
