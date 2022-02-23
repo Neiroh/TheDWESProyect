538af3d7539391b0fa61e76241c50019b93f44e6
Neiroh <raulito72001@gmail.com>
2022-02-23 17:26:16 +0100

    movido

--- GalorWeb/app/crud.php
+++ GalorWeb/app/crud.php
@@ -3,6 +3,13 @@
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
 
-    $db = new mysqli('localhost', 'galorDB', '')
+    $db = new mysqli('localhost', 'galorDB', 'arce', '123456');
+
+    function creaUser($nombre, $correo, $passwd){
+
+        $contra = hash('sha256', $passwd, false);
+        $db->query("INSERT INTO user('nombreUser', 'passwd', 'correoUser') VALUES ($nombre, $correo, $contra)");
+
+    }
 
 ?>
--- galorDB.sql
+++ galorDB.sql
@@ -4,9 +4,6 @@ USE galorDB;
 CREATE TABLE IF NOT EXISTS user(
 	idUser INT PRIMARY KEY AUTO_INCREMENT,
     nombreUser VARCHAR(100) NOT NULL,
-    ap1 VARCHAR(100) NOT NULL,
-    ap2 VARCHAR(100),
-    nickname VARCHAR(25) NOT NULL,
     passwd VARCHAR(25) NOT NULL,
     correoUser VARCHAR(200) NOT NULL,
     fotoPerfil LONGBLOB
