06890632c253b070ae5986b1e2b5f69e05f00c2e
Neiroh <raulito72001@gmail.com>
2022-02-23 09:17:51 +0100

    esqueleto index

---
+++ code/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galor</title>
+</head>
+<body>
+    
+    <header>
+
+        <img class="logo" src="" alt="">
+        <h1>Galor</h1>
+
+    </header>
+
+    <main>
+
+        <nav>
+
+            <div class="botonRegistro"><p>Regístrate</p></div>
+            <div class="botonInicio"><p>Iniciar Sesión</p></div>
+
+        </nav>
+
+        <div class="buscador">
+
+            <form action="./buscador" method="get" value="Color">
+
+                <input type="text" class="buscador">
+
+                <select name="categoria" id="categoria">
+
+                    <option value="azul">Azul</option>
+                    <option value="verde">Verde</option>
+                    <option value="rojo">Rojo</option>
+                    <option value="negro">Negro</option>
+                    <option value="blanco">Blanco</option>
+                    <option value="rosa">Rosa</option>
+                    <option value="gris">Gris</option>
+
+                </select>
+
+                <input type="submit" class="enviaBuscador" value="Buscar">
+
+            </form>
+
+        </div>
+
+        <div class="contenedorImagenes">
+
+            
+
+        </div>
+
+    </main>
+
+    <footer>
+
+
+
+    </footer>
+
+</body>
+</html>
