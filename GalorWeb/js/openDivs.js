992b0ee4fe5e39f9b5d9894e8acd7c5513622482
AhmedMA0 <10253@iespuertasdelcampo.es>
2022-02-25 08:45:21 +0100

    openDivs2

--- GalorWeb/js/openDivs.js
+++ GalorWeb/js/openDivs.js
@@ -1,9 +1,31 @@
 
 reg.addEventListener('click', ()=>{
-    divR.style.display = 'block';
+    if (divR.style.display == 'block') {
+        divR.style.display = 'none';
+    }
+
+    else{
+        divR.style.display = 'block';
+    }
+
+    if (divL.style.display == 'block') {
+        divL.style.display = 'none';
+    }
+    
 });
 
 
 log.addEventListener('click', ()=>{
-    divL.style.display = 'block';
+    if (divL.style.display == 'block') {
+        divL.style.display = 'none';
+    }
+
+    else{
+        divL.style.display = 'block';
+    }
+
+    if (divR.style.display == 'block') {
+        divR.style.display = 'none';
+    }
+
 });
