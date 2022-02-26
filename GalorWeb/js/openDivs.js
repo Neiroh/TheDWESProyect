2530162ead4b239bee0172ac44bc7cc7dbbaf9c3
Neiroh <raulito72001@gmail.com>
2022-02-26 16:29:57 +0100

    arreglo opendivs.js

--- GalorWeb/js/openDivs.js
+++ GalorWeb/js/openDivs.js
@@ -2,11 +2,9 @@ reg.addEventListener('click', () => {
     if (divR.style.display == 'block') {
         divR.style.display = 'none';
         capaOscura.style.display = "none";
-        html.style.overflow = "visible";
     } else {
         divR.style.display = 'block';
         capaOscura.style.display = "block";
-        html.style.overflow = "hidden";
     }
 
     if (divL.style.display == 'block') {
@@ -19,11 +17,9 @@ log.addEventListener('click', () => {
     if (divL.style.display == 'block') {
         divL.style.display = 'none';
         capaOscura.style.display = "none";
-        html.style.overflow = "visible";
     } else {
         divL.style.display = 'block';
         capaOscura.style.display = "block";
-        html.style.overflow = "hidden";
     }
 
     if (divR.style.display == 'block') {
