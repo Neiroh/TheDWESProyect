ba1a57ac9bacc3acb2d8f13c0aeeac54aa65e463
JesusRO0 <6193@iespuertasdelcampo.es>
2022-02-26 18:12:00 +0100

    Estilo de la foto

--- GalorWeb/foto.php
+++ GalorWeb/foto.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foto</title>
+    <link rel="stylesheet" href="./css/foto.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"> 
 </head>
 
 <body>
@@ -13,60 +20,56 @@
     <header>
 
         <div class="logo">
-            <img src="" alt="logo">
+            <img src="images/camara.png" alt="logo">
             <h1>Galor</h1>
         </div>
 
-        <div class="perfil">
-            <h3>Nombre Perfil</h3>
-            <input type="button" name="perfil" id="perfil" value="V">
-            <img src="" alt="foto_perfil" class="fotoPerfilTop">
-        </div>
-
-        <div class="sesion">
-
-            <div class="boton registro">Regístrate</div>
-            <div class="boton inicioSesion">Iniciar Sesión</div>
-
-        </div>
-
     </header>
-
+    <hr>
     <main>
 
         <div class="info">
 
-            <img src="" alt="foto" class="bigFoto">
-            <h2>Título de la foto</h2>
+            <div class="fotoTitulo">
+                <img src="images/paisaje.jpg" alt="foto" class="bigFoto">
+                <div class="titulo">
+                    <h2>Camino a la libertad</h2>
+                    <hr>
+                    <p class="descripcion">El lugar produce sensación de quietud,
+                                            calma, tranquilidad. También de soledad,
+                                            pues no se ve ningún rastro de seres
+                                            animados en todo él. Puede parecer que
+                                            se acaba el verano y llegan los primeros
+                                            días del cambio de estación, de una
+                                            manera serena y absolutamente natural.
+                    </p>
+                    <div class="likes">
+                        <img src="images/megusta.png" alt="foto" class="megusta">
+                        <p class="numLikes">32</p>
+                    </div>
+                </div>
+            </div>
 
             <div class="separador"></div>
 
-            <p class="descripcion"></p>
+            <div class="comentarios">
+                <h3>Comentarios</h3>
+                <hr>
 
-            <div class="likes">
-
-                <p class="cora"></p>
-                <p class="numLikes"></p>
-
+                <div class="usuario">
+                    <img src="images/camara.png" alt="perfil">
+                    <h3>ARCE69</h3>
+                    <input type="text" name="comentario" id="comentario" placeholder="Escribe tu comentario público">
+                </div> 
             </div>
 
         </div>
-
-        <div class="comentarios">
-
-            <h3>Comentarios</h3>
-            <div class="separador"></div>
-
-            <!--
-                <div class="usuario">
-                    <img src="" alt="perfil">
-                    <h3></h3>
-                    <input type="text" name="comentario" id="comentario" placeholder="Escribe tu comentario público">
-                </div>
-            -->
-
-        </div>
     </main>
+
+    <footer>
+    <hr>
+        <h3>2º Desarrollo de Aplicaciones Web en Entorno Servidor</h3>
+    </footer>
 
 </body>
 
