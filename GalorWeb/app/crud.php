388355c8a81ca805e51293ee1b3f5d599daf3456
Neiroh <raulito72001@gmail.com>
2022-02-27 18:34:13 +0100

    función compruebaLike

--- GalorWeb/app/crud.php
+++ GalorWeb/app/crud.php
@@ -372,4 +372,35 @@
 
         $db->query("DELETE FROM likes WHERE idImg = '$idImg' AND idUser = '$idUser'");
     }
+
+    function compruebaLike($idImg, $idUser){
+
+        try{
+            $db = new mysqli('localhost', "ahmed", "123456", "galorDB");
+
+            if($db->connect_errno){
+
+                throw new Exception("No se ha podido acceder a la basede datos");
+
+            }
+        }catch(Exception $ex){
+
+            echo $ex->getMessage(), "<br>";
+
+        }
+
+        $consulta = $db->query("SELECT $idUser FROM likes WHERE idUser = '$idUser' AND idImg = '$idImg'");
+        $ejecuta = $consulta->fetch_object();
+
+        if($ejecuta){
+
+            return true;
+
+        }else{
+
+            return false;
+
+        }
+
+    }
 ?>
